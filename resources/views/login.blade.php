7bc256251cd1666c5aae866e5017d03b21dc3ccf
Tiendzok <nvtien163@gmail.com>
2026-06-04 13:27:04 +0700

    As

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,19 +7,19 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset('/css/login.css') }}" rel="stylesheet" />
     <title>Login</title>
-    </head>
+</head>
 
 <body>
     <main>
         <section class="mri">
-            <form method="POST" action="{{ route('login') }}" role="form">
+            <form method="POST" action="{{ secure_url('/login') }}" role="form">
                 <h1>Đăng nhập</h1>
                 @csrf
-                
+
                 @if (session('error'))
-                    <div style="color: red; margin-bottom: 10px; text-align: center;">
-                        {{ session('error') }}
-                    </div>
+                <div style="color: red; margin-bottom: 10px; text-align: center;">
+                    {{ session('error') }}
+                </div>
                 @endif
 
                 <div class="un">
@@ -27,7 +27,7 @@
                     <input type="text" id="email" placeholder="Nhập Email" name="email" value="{{ old('email') }}">
                 </div>
                 @if ($errors->has('email'))
-                    <span class="error-message" style="color: red; font-size: 12px;">{{ $errors->first('email') }}</span>
+                <span class="error-message" style="color: red; font-size: 12px;">{{ $errors->first('email') }}</span>
                 @endif
 
                 <div class="pn">
@@ -35,7 +35,7 @@
                     <input type="password" id="password" placeholder="Nhập mật khẩu" name="password">
                 </div>
                 @if ($errors->has('password'))
-                    <span class="error-message" style="color: red; font-size: 12px;">{{ $errors->first('password') }}</span>
+                <span class="error-message" style="color: red; font-size: 12px;">{{ $errors->first('password') }}</span>
                 @endif
 
                 <button type="submit" style="margin-top: 20px;">Login</button>
@@ -44,15 +44,17 @@
     </main>
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    
+
     @if(session('error'))
     <script>
-        Swal.fire({
-            icon: 'error',
-            title: 'Lỗi...',
-            text: '{{ session('error') }}',
-        })
+    Swal.fire({
+        icon: 'error',
+        title: 'Lỗi...',
+        text: '{{ session('
+        error ') }}',
+    })
     </script>
     @endif
 </body>
+
 </html>
